7b84b7024806cc7b2f1a50d45b5aaceaf8afc24e
Kenton Lewis <kentonp.tech@gmail.com>
2015-04-22 09:43:54 -0400

    Page Cleanup
    - Removed extra text and centered index page
    - Removed extra text and centered franchise landing page

--- franchises/franchise_landing.php
+++ franchises/franchise_landing.php
@@ -17,20 +17,14 @@
     </head>
     <body>
         <div id="wrapper">
-
-            <!-- Sidebar -->
             <?php include ('../nav/franchiseTopSidebar.php'); ?>
-            <!-- /#sidebar-wrapper -->
-
-            <!-- Page Content -->
-            <div id="page-content-wrapper">
+            <div id="page-content-wrapper" style="text-align: center">
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-lg-12">
                             <h1>Franchises</h1>
-                            <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                            <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
-                            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                            <br>
+                            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Navigation</a>
                         </div>
                     </div>
                 </div>
--- index.php
+++ index.php
@@ -18,13 +18,12 @@
     <body>
         <div id="wrapper">
             <?php include ('nav/topLevelNav.php'); ?>
-            <div id="page-content-wrapper">
+            <div id="page-content-wrapper" style="text-align : center">
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-lg-12">
                             <h1>Home</h1>
-                            <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                            <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                            <br>
                             <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Collapse Navigation</a>
                         </div>
                     </div>
