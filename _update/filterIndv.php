c7a3f622881e3186a7e7f56e6285720a73e7395c
Kenton Lewis <kentonp.tech@gmail.com>
2015-10-15 09:52:42 -0400

    Statistics Page v1
    - First pass at a franchise statistics page

---
+++ _update/filterIndv.php
@@ -0,0 +1,16 @@
+<?php
+
+$team = $_POST['team'];
+
+$con = mysql_connect("localhost", 'root', 'Fly0Bird797979');
+
+if (!$con) {
+    die('Could not connect!' . mysql_error());
+}
+
+mysql_select_db("madden08_db", $con);
+
+$UpdateTeamFilter = mysql_query("UPDATE `stat_controls` set Value='{$team}' WHERE Control='IndvStatsFilter'", $con);
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+
