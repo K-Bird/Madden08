e4fe359e478b838e895e350d3e936c348f482447
Kenton Lewis <kentonp.tech@gmail.com>
2016-05-05 21:32:46 -0400

    Fix - Results Bye
    - Can now add BYE to results vs value

--- libs/ajax/franchise_view/results/update_franchise_results_VsTeam.php
+++ libs/ajax/franchise_view/results/update_franchise_results_VsTeam.php
@@ -4,8 +4,12 @@ include ($_SERVER['DOCUMENT_ROOT'] . "/Madden08/libs/db/common_db_functions.php"
 $new_team_value = $_POST['team'];
 $row = $_POST['row'];
 
+if ($new_team_value === 'BYE') {
+    $Update_VsAt = db_query("Update `franchise_year_results` set `Vs`='{$new_team_value}' Where Row='{$row}'");
+} else {
 $getTeamMascot = db_query("SELECT * FROM `franchise_info` WHERE Franchise='{$new_team_value}'");
 $MascotRow = $getTeamMascot->fetch_assoc();
 $Mascot = $MascotRow['Mascot'];
 
 $Update_VsAt = db_query("Update `franchise_year_results` set `Vs`='{$Mascot}' Where Row='{$row}'");
+}
